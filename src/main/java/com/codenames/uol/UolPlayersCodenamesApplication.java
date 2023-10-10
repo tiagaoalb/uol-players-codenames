c08cf5243fb4738e2bd2a78fd29e4dd737bce60f
Tiago Bovolin <tiagaoalb@gmail.com>
2023-10-09 22:11:45 -0300

    Initial commit

---
+++ src/main/java/com/codenames/uol/UolPlayersCodenamesApplication.java
@@ -0,0 +1,13 @@
+package com.codenames.uol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UolPlayersCodenamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UolPlayersCodenamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codenames/uol/UolPlayersCodenamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codenames.uol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UolPlayersCodenamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
